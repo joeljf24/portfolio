33babfb21015702bc81e2b129655e97b193259bd
joeljf24 <u.programming.jf@gmail.com>
2025-03-21 11:52:21 -0300

    Cleaning repository

--- src/app/(routes)/experience/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Transition } from '@/components/Transition/Transition'
-
-export default function ExperiencePage() {
-  return (
-    <div className='min-h-screen'>
-        <Transition />
-    </div>
-  )
-}
